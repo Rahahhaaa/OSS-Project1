7f51190eecc43831d1527fb47abdbef56cc0dcd5
Junseok <41678838+Rahahhaaa@users.noreply.github.com>
2022-11-14 02:55:37 +0900

    Add files via upload

---
+++ CalendarCrawling.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+undergraduate_schedule = []
+graduate_schedule = []
+
+undergraduate_schedule_date = []
+graduate_schedule_date = []
+
+url = 'https://www.chungbuk.ac.kr/site/www/sub.do?key=1853'
+headers = {'User->Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+res = requests.get(url, headers)
+soup = BeautifulSoup(res.content, 'lxml')
+
+data = soup.select('div.schedule')
+
+i = 0
+for item in data:
+    str[i] = item.get_text()
+    i = i + 1
+    
+for i in len(str):
+    print(str[i])
